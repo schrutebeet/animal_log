907a9d9e4301d32d4a871ded18667680d0d708d4
Ricardo Sans <ricardosans98@gmail.com>
2024-03-21 08:00:25 +0100

    Erase url from file

--- animal_logger/src/db/connection.py
+++ animal_logger/src/db/connection.py
@@ -3,18 +3,11 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from config.config import Config
 
-# used to construct the database connection URL
-__DATABASE_URL = (
-    f"postgresql://{Config.get_info()['user']}:"
-    f"{Config.get_info()['password']}@"
-    f"{Config.get_info()['host']}/"
-    f"{Config.get_info()['database']}"
-)
 
 # used to create a database engine
 # the engine manages the database connection.
 # The echo=True argument is optional and is often used for debugging purposes (turned off)
-engine = create_engine(__DATABASE_URL)
+engine = create_engine(Config.get_info()['db_url'])
 
 # used to create a models (tables) based on an object-oriented approach
 Base = declarative_base()
