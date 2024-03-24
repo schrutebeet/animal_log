ad189bd2ec297683ec7031ce58ce935e562b3660
schrutebeet <ricardosans98@gmail.com>
2024-03-24 19:13:16 +0100

    Create main file

---
+++ animal_logger/src/main.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk
+
+from config.config import Config
+from animal_logger.src.frames.login_page import LoginPage
+from animal_logger.src.frames.dashboard import Dashboard
+
+
+if __name__ == "__main__":
+    from config.config import Config
+    Config()
+    window = ctk.CTk() # Creates window
+    # LoginPage(window).initialize_ui()
+    Dashboard(window).initialize_ui()
