a44fc2f81b917a432f81b3ad00834e1bc304f2f0
Ricardo Sans <ricardosans98@gmail.com>
2025-03-27 23:18:36 +0100

    improve UI for animal class

--- animal_logger/src/frames/add_animal.py
+++ animal_logger/src/frames/add_animal.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 import customtkinter as ctk
 
@@ -7,19 +8,28 @@ from animal_logger.src.frames.baseframe import BaseFrame
 
 
 class AddAnimal(BaseFrame):
+    
+    _is_ui_initialized = False
 
     def __init__(self, window, main_frame: ctk.CTkFrame) -> None:
         super().__init__(window)
         self.main_frame = main_frame
+        self.main_frame.pack(expand=True)  
         self.image_pool = Path(add_animal.__file__).parent
 
     def initialize_ui(self) -> None:
-        self.choose_class_of_animal()
+        if not self._is_ui_initialized:
+            self.choose_class_of_animal()
 
     def choose_class_of_animal(self):
         animal_classes = Config.get_info()['animal_classes']
+        len_animal_classes = len(animal_classes)
         animal_class_colors = ['#EFBC9B', '#FFE4CF', '#FFEDD8', '#F1F5A8', '#78A083', '#B7C9F2']
-        for animal_class, color in zip(animal_classes, animal_class_colors):
+        num_cols = 3
+        num_rows = math.ceil(len_animal_classes / num_cols)
+        col_list = [col for col in range(num_cols)] * num_rows
+        row_list = [row for row in range(num_rows) for _ in range(num_cols)]
+        for animal_class, color, col, row in zip(animal_classes, animal_class_colors, col_list, row_list):
             button = ctk.CTkButton(self.main_frame,
                                    text = animal_class.capitalize(),
                                    image = self.get_image(self.image_pool, animal_class),
@@ -27,9 +37,12 @@ class AddAnimal(BaseFrame):
                                    bg_color = color,
                                    hover_color = color,
                                    text_color = 'white',
-                                   command = getattr(self, 'set_' + animal_class + '_button'))
-            button.pack(fill = 'x', padx = 20, pady = 10)
+                                   command = getattr(self, 'set_' + animal_class + '_button',),
+                                   height = 100,
+                                   width = 100)
+            button.grid(row = row, column = col, padx = 5, pady = 5, sticky = 'nsew', in_ = self.main_frame)
             setattr(self, animal_class, button)
+        AddAnimal._is_ui_initialized = True
 
     def set_mammal_button(self):
         pass
--- animal_logger/src/main.py
+++ animal_logger/src/main.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     Config()
     window = ctk.CTk() # Creates window
     LoginPage(window).initialize_ui()
-    
+    Dashboard(window).initialize_ui()
